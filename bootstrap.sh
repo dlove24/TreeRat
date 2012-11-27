a4f9bf25af9016c1e9c0c74e0c2c0f6615fedb32
David Love <david@homeunix.org.uk>
2012-11-27 16:50:22 +0000

    Set-up the C compiler for C99 and C++ compilers for C++11

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,8 +4,8 @@
 rm -rf CMakeCache.txt
 
 # Set-up the environment
-export CC=/usr/bin/clang
-export CXX=/usr/bin/clang++ 
+export CC=/usr/bin/clang 
+export CXX=/usr/bin/clang++
 
 # Re-run CMake
 mkdir -p build
