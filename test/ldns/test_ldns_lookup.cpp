5c14fc3c731a54d3bbbf33927e03749b8a830da7
David Love <david@homeunix.org.uk>
2012-12-01 22:30:01 +0000

    Add the compatability headers to the lDNS library to allow sucessfull link.

--- test/ldns/test_ldns_lookup.cpp
+++ test/ldns/test_ldns_lookup.cpp
@@ -35,17 +35,30 @@
 using namespace std;
 using namespace TAP;
 
-int main (int argc, char** argv) {
+int main () {
   ldns_resolver* res;
   ldns_rdf* domain;
   ldns_pkt* p;
   ldns_rr_list* mx;
   ldns_status s;
 
+  //domain = ldns_dname_new_frm_str("time.aaisp.net.uk");
+  domain = ldns_dname_new_frm_str("www.google.com");
+  s = ldns_resolver_new_frm_file(&res, NULL);
+  p = ldns_resolver_query(res, domain, LDNS_RR_TYPE_A, LDNS_RR_CLASS_IN, LDNS_RD);
+  mx = ldns_pkt_rr_list_by_type(p, LDNS_RR_TYPE_A, LDNS_SECTION_ANSWER);
+
+  ldns_rr_list_sort(mx);
+  ldns_rr_list_print(stdout, mx);
+
+  ldns_rr_list_deep_free(mx);
+  ldns_pkt_free(p);
+  ldns_resolver_deep_free(res);
+
   plan (2);
 
-  isnt (run (prog_location, ""), "", "No output when called with no arguments");
-  isnt (check_status (prog_location, ""), 0, "Exit status is zero when called with no arguments");
+  //isnt (p, "", "No output when called with no arguments");
+  //isnt (p, 0, "Exit status is zero when called with no arguments");
 
   return exit_status();
   }
