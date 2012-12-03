531186b3b00f4ac3979b8580bb1308357f73d29a
David Love <david@homeunix.org.uk>
2012-12-03 10:05:25 +0000

    Unlink the lDNS library test test_ldns_00 from the build. The output is not stable, and we will test this library indirectly through the DNS helper

--- test/ldns/test_ldns_lookup.cpp
+++ test/ldns/test_ldns_lookup.cpp
@@ -39,7 +39,7 @@ using namespace std;
 using namespace TAP;
 
 int main () {
-    char format_str[] = "address";
+  char format_str[] = "address";
   const ldns_output_format   test_dns_output_format_record = { LDNS_COMMENT_KEY | LDNS_COMMENT_BUBBLEBABBLE | LDNS_COMMENT_FLAGS, &format_str };
   const ldns_output_format*  test_dns_output_format = &test_dns_output_format_record;
 
@@ -53,59 +53,59 @@ int main () {
 
   // Try an A record lookup against a known target
 
-  domain = ldns_dname_new_frm_str("www.homeunix.org.uk");
-  s = ldns_resolver_new_frm_file(&res, NULL);
-  p = ldns_resolver_query(res, domain, LDNS_RR_TYPE_A, LDNS_RR_CLASS_IN, LDNS_RD);
-  mx = ldns_pkt_rr_list_by_type(p, LDNS_RR_TYPE_A, LDNS_SECTION_ANSWER);
+  domain = ldns_dname_new_frm_str ("www.homeunix.org.uk");
+  s = ldns_resolver_new_frm_file (&res, NULL);
+  p = ldns_resolver_query (res, domain, LDNS_RR_TYPE_A, LDNS_RR_CLASS_IN, LDNS_RD);
+  mx = ldns_pkt_rr_list_by_type (p, LDNS_RR_TYPE_A, LDNS_SECTION_ANSWER);
 
-  ldns_rr_list_sort(mx);
-  is (ldns_rr_list2str_fmt(test_dns_output_format, mx), "www.homeunix.org.uk.	86400	IN	A	81.187.233.188", "Unexpected return for Homeunix A record");
+  ldns_rr_list_sort (mx);
+  is (ldns_rr_list2str_fmt (test_dns_output_format, mx), "www.homeunix.org.uk.	86400	IN	A	81.187.233.188", "Unexpected return for Homeunix A record");
 
-  ldns_rr_list_deep_free(mx);
-  ldns_pkt_free(p);
-  ldns_resolver_deep_free(res);
+  ldns_rr_list_deep_free (mx);
+  ldns_pkt_free (p);
+  ldns_resolver_deep_free (res);
 
   // Try an AAAA record lookup against a known target
 
-  domain = ldns_dname_new_frm_str("www.homeunix.org.uk");
-  s = ldns_resolver_new_frm_file(&res, NULL);
-  p = ldns_resolver_query(res, domain, LDNS_RR_TYPE_AAAA, LDNS_RR_CLASS_IN, LDNS_RD);
-  mx = ldns_pkt_rr_list_by_type(p, LDNS_RR_TYPE_AAAA, LDNS_SECTION_ANSWER);
+  domain = ldns_dname_new_frm_str ("www.homeunix.org.uk");
+  s = ldns_resolver_new_frm_file (&res, NULL);
+  p = ldns_resolver_query (res, domain, LDNS_RR_TYPE_AAAA, LDNS_RR_CLASS_IN, LDNS_RD);
+  mx = ldns_pkt_rr_list_by_type (p, LDNS_RR_TYPE_AAAA, LDNS_SECTION_ANSWER);
 
-  ldns_rr_list_sort(mx);
-  is (ldns_rr_list2str_fmt(test_dns_output_format, mx), "www.homeunix.org.uk.	86400	IN	AAAA	2001:8b0:1698:cf71::50:0", "Unexpected return for Homeunix A record");
+  ldns_rr_list_sort (mx);
+  is (ldns_rr_list2str_fmt (test_dns_output_format, mx), "www.homeunix.org.uk.	86400	IN	AAAA	2001:8b0:1698:cf71::50:0", "Unexpected return for Homeunix A record");
 
-  ldns_rr_list_deep_free(mx);
-  ldns_pkt_free(p);
-  ldns_resolver_deep_free(res);
+  ldns_rr_list_deep_free (mx);
+  ldns_pkt_free (p);
+  ldns_resolver_deep_free (res);
 
   // Try an MX record lookup against a known target
 
-  domain = ldns_dname_new_frm_str("homeunix.org.uk");
-  s = ldns_resolver_new_frm_file(&res, NULL);
-  p = ldns_resolver_query(res, domain, LDNS_RR_TYPE_MX, LDNS_RR_CLASS_IN, LDNS_RD);
-  mx = ldns_pkt_rr_list_by_type(p, LDNS_RR_TYPE_MX, LDNS_SECTION_ANSWER);
+  domain = ldns_dname_new_frm_str ("homeunix.org.uk");
+  s = ldns_resolver_new_frm_file (&res, NULL);
+  p = ldns_resolver_query (res, domain, LDNS_RR_TYPE_MX, LDNS_RR_CLASS_IN, LDNS_RD);
+  mx = ldns_pkt_rr_list_by_type (p, LDNS_RR_TYPE_MX, LDNS_SECTION_ANSWER);
 
-  ldns_rr_list_sort(mx);
-  is (ldns_rr_list2str_fmt(test_dns_output_format, mx), "homeunix.org.uk.	86400	IN	MX	10 hotmail.homeunix.org.uk.", "Unexpected return for Homeunix A record");
+  ldns_rr_list_sort (mx);
+  is (ldns_rr_list2str_fmt (test_dns_output_format, mx), "homeunix.org.uk.	86400	IN	MX	10 hotmail.homeunix.org.uk.", "Unexpected return for Homeunix A record");
 
-  ldns_rr_list_deep_free(mx);
-  ldns_pkt_free(p);
-  ldns_resolver_deep_free(res);
+  ldns_rr_list_deep_free (mx);
+  ldns_pkt_free (p);
+  ldns_resolver_deep_free (res);
 
   // Lookup a known invalid host
 
-  domain = ldns_dname_new_frm_str("invalid-host.homeunix.org.uk");
-  s = ldns_resolver_new_frm_file(&res, NULL);
-  p = ldns_resolver_query(res, domain, LDNS_RR_TYPE_MX, LDNS_RR_CLASS_IN, LDNS_RD);
-  mx = ldns_pkt_rr_list_by_type(p, LDNS_RR_TYPE_MX, LDNS_SECTION_ANSWER);
+  domain = ldns_dname_new_frm_str ("invalid-host.homeunix.org.uk");
+  s = ldns_resolver_new_frm_file (&res, NULL);
+  p = ldns_resolver_query (res, domain, LDNS_RR_TYPE_MX, LDNS_RR_CLASS_IN, LDNS_RD);
+  mx = ldns_pkt_rr_list_by_type (p, LDNS_RR_TYPE_MX, LDNS_SECTION_ANSWER);
 
-  ldns_rr_list_sort(mx);
-  is (ldns_rr_list2str_fmt(test_dns_output_format, mx), "", "Found record for an invalid host");
+  ldns_rr_list_sort (mx);
+  is (ldns_rr_list2str_fmt (test_dns_output_format, mx), "", "Found record for an invalid host");
 
-  ldns_rr_list_deep_free(mx);
-  ldns_pkt_free(p);
-  ldns_resolver_deep_free(res);
+  ldns_rr_list_deep_free (mx);
+  ldns_pkt_free (p);
+  ldns_resolver_deep_free (res);
 
   return exit_status();
   }
